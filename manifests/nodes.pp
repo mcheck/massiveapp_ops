d6a2a07016a33d3e4cbbf65132f7576600fcf6b8
mcheck <mcheck@sologroup.com>
2012-02-12 20:53:14 -0600

    Add the node file to manage hosts

---
+++ manifests/nodes.pp
@@ -0,0 +1,8 @@
+
+node "app" {
+	include apache2
+	include mysql
+	include massiveapp
+	include passenger
+
+}
--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,4 @@ Exec {
   path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 }
 
-include apache2
-include mysql
-include massiveapp
-include passenger
+import "nodes.pp"
